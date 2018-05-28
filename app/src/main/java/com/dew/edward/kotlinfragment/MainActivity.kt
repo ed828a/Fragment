9b0ce4a060ccfc57e48efa9084fc2f0a7d58f13f
Edward <ed828a@gmail.com>
2018-05-28 17:08:29 +0930

    Add ToolBar

--- app/src/main/java/com/dew/edward/kotlinfragment/MainActivity.kt
+++ app/src/main/java/com/dew/edward/kotlinfragment/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.dew.edward.kotlinfragment
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.view.Menu
 import android.view.MenuItem
 import com.dew.edward.kotlinfragment.fragments.FirstImageFragment
@@ -12,14 +14,32 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val drawerToggle by lazy {
+        ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.drawer_open, R.string.drawer_close)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolbar)
+        drawerLayout.addDrawerListener(drawerToggle)
         navigationView.setNavigationItemSelectedListener {
             selectDrawerItem(it)
             true
         }
+    }
+
+    // configure configuration and synchronization between drawToggle and drawLayout
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        drawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        drawerToggle.onConfigurationChanged(newConfig)
     }
 
     private fun selectDrawerItem(item: MenuItem){
@@ -55,21 +75,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        return when (item?.itemId) {
-            R.id.firstFragmentItem -> {
-                val fragment = FirstImageFragment.newInstance()
-                replaceFragment(fragment)
-                true
-            }
-
-            R.id.secondFragmentItem -> {
-                val fragment = SecondImageFragment.newInstance()
-                replaceFragment(fragment)
-                true
-            }
-
-            else -> super.onOptionsItemSelected(item)
-
-        }
+//        return when (item?.itemId) {
+//            R.id.firstFragmentItem -> {
+//                val fragment = FirstImageFragment.newInstance()
+//                replaceFragment(fragment)
+//                true
+//            }
+//
+//            R.id.secondFragmentItem -> {
+//                val fragment = SecondImageFragment.newInstance()
+//                replaceFragment(fragment)
+//                true
+//            }
+//
+//            else -> super.onOptionsItemSelected(item)
+//
+//        }
+        // disable onOptionsItemSelected
+        return if (drawerToggle.onOptionsItemSelected(item)) true
+        else super.onOptionsItemSelected(item)
     }
 }
